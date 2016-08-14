7b9d8dbddd7af5563af23cdd34d41a4c4e75dff2
Hans <imorgenerdetmandag@gmail.com>
2016-08-15 00:48:13 +0200

    minor fixes

--- src/Commands/Bedtime.java
+++ src/Commands/Bedtime.java
@@ -1,11 +1,5 @@
 package Commands;
 
-import org.jsoup.Jsoup;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
